69fb24467bd3ef9d9856dd7dfeae6f5bd2632c52
NicsssXO <nicmontoya28@gmail.com>
2025-02-15 12:43:28 -0700

    updating readme

--- adventure_log.py
+++ adventure_log.py
@@ -2,5 +2,4 @@ def greet_coding_temple():
     print ("Ahoy, Coding Temple Navigators! Are you ready to adventure?")
 
   if  __name__ == "__main__":
-    greet_coding_temple
-    
+    greet_coding_temple()
